ef51e9aa649e6aaaef0e530c1fabd24633df2b4d
Jerrywang959 <jerryw99@foxmail.com>
2022-08-14 16:09:40 +0000

    🎉 init: init via graph cli

---
+++ src/campaign-factory-upgradable.ts
@@ -0,0 +1,12 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+import {
+  CampaignFactoryUpgradable,
+  AdminChanged,
+  BeaconUpgraded,
+  Upgraded,
+} from "../generated/CampaignFactoryUpgradable/CampaignFactoryUpgradable";
+import { User } from "../generated/schema";
+
+export function handleCampaignCreated(): void {}
+
+export function handleWhiteTokenSet(): void {}

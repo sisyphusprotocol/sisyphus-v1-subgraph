35bff10127874641a9890d04dd2380496eb4141c
Jerrywang959 <jerryw99@foxmail.com>
2022-08-20 19:24:38 +0000

    🐳 chore: add ignore list

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,5 @@
 // used in test, ignore non-compatible address
 export const campaignIgnoredList = [
   "0x009c1546cfbee74858fe3d11446555ce86c2c036",
+  "0x4701e3f4e9e95690d4615b1c406e6e59441b20f3",
 ];
